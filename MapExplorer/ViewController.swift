939d35ab621f587ee04788d84d6a41570f1bc306
Ilya.Rudenko <ilya.rudenko@waveaccess.global>
2022-05-15 14:00:52 +0300

    Initial project

---
+++ MapExplorer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MapExplorer
+//
+//  Created by Ilya on 15.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
