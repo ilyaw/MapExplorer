373bf4101191f68ec0b40fd024e4ca6e98cb18ff
Ilya.Rudenko <ilya.rudenko@waveaccess.global>
2022-07-01 17:05:57 +0300

    Добавил шторку, поправил поля

--- MapExplorer/AppDelegates/SceneDelegate.swift
+++ MapExplorer/AppDelegates/SceneDelegate.swift
@@ -13,7 +13,39 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
     
     private var appRouter: AppRouterDelegate?
-    private let container: Container = {
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: windowScene)
+        
+        let container = registrationDI()
+        appRouter = container.resolve(AppRouterDelegate.self, argument: window)
+        appRouter?.start()
+    }
+
+    func sceneDidDisconnect(_ scene: UIScene) {
+        
+    }
+
+    func sceneDidBecomeActive(_ scene: UIScene) {
+        appRouter?.setBlind(false)
+    }
+
+    func sceneWillResignActive(_ scene: UIScene) {
+        appRouter?.setBlind(true)
+    }
+
+    func sceneWillEnterForeground(_ scene: UIScene) {
+        
+    }
+
+    func sceneDidEnterBackground(_ scene: UIScene) {
+        
+    }
+}
+
+private extension SceneDelegate {
+    func registrationDI() -> Container {
         let container = Container()
         // Карта
         container.register(MapViewController.self) { _ in
@@ -49,48 +81,15 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
             return controller
         }
         
+        // Шторка для приватной информации
+        container.register(BlindViewController.self) { _ in BlindViewController() }
+        
         // Роутер
         container.register(AppRouterDelegate.self) { (_, window: UIWindow?) in
             let controller = AppRouter(window: window, container: container)
             return controller
         }
-        return container
-    }()
-
-    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let windowScene = (scene as? UIWindowScene) else { return }
-        window = UIWindow(windowScene: windowScene)
         
-        appRouter = container.resolve(AppRouterDelegate.self, argument: window)
-        appRouter?.start()
+        return container
     }
-
-    func sceneDidDisconnect(_ scene: UIScene) {
-        // Called as the scene is being released by the system.
-        // This occurs shortly after the scene enters the background, or when its session is discarded.
-        // Release any resources associated with this scene that can be re-created the next time the scene connects.
-        // The scene may re-connect later, as its session was not necessarily discarded (see `application:didDiscardSceneSessions` instead).
-    }
-
-    func sceneDidBecomeActive(_ scene: UIScene) {
-        // Called when the scene has moved from an inactive state to an active state.
-        // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
-    }
-
-    func sceneWillResignActive(_ scene: UIScene) {
-        // Called when the scene will move from an active state to an inactive state.
-        // This may occur due to temporary interruptions (ex. an incoming phone call).
-    }
-
-    func sceneWillEnterForeground(_ scene: UIScene) {
-        // Called as the scene transitions from the background to the foreground.
-        // Use this method to undo the changes made on entering the background.
-    }
-
-    func sceneDidEnterBackground(_ scene: UIScene) {
-        // Called as the scene transitions from the foreground to the background.
-        // Use this method to save data, release shared resources, and store enough scene-specific state information
-        // to restore the scene back to its current state.
-    }
-
 }
